1f849049130651045d827371bf6cf46f50a10f4f
evjeniy <metall.92@inbox.ru>
2021-01-19 22:56:16 +0200

    initial commit

--- dist/assets/js/calculator.js
+++ dist/assets/js/calculator.js
@@ -1,202 +1,205 @@
-var $, Calculator;
+(function() {
+  var $, Calculator;
 
-$ = jQuery;
+  $ = jQuery;
 
-$.fn.calculator = function(options = {}) {
-  return this.each(function() {
-    var data, self;
-    self = $(this);
-    data = self.data('calculator');
-    if (!data) {
-      self.data('calculator', new Calculator(this, options));
-    }
-    if (typeof options === 'string') {
-      return data[options]();
-    }
-  });
-};
-
-Calculator = class Calculator {
-  constructor(dom, options = {}) {
-    var defaults;
-    this.dom = dom;
-    defaults = {
-      step: ".step",
-      next: ".next",
-      prev: ".prev",
-      retreat: ".retreat",
-      progress: {
-        controls: ".component-calculator__forms_controls",
-        current: ".component-calculator__forms_controls .current",
-        total: ".component-calculator__forms_controls .total"
-      },
-      dots: {
-        container: '.steps-dots',
-        item: 'span'
+  $.fn.calculator = function(options = {}) {
+    return this.each(function() {
+      var data, self;
+      self = $(this);
+      data = self.data('calculator');
+      if (!data) {
+        self.data('calculator', new Calculator(this, options));
       }
-    };
-    this.options = $.extend(defaults, options);
-    this._setup();
-  }
-
-  _setup() {
-    this.container = $(this.dom);
-    this.initialStep = this.container.find(`${this.options.step}.active`);
-    this.steps = this.container.find(this.options.step);
-    this.wizard = this.initialStep.find(".step-form input:checked").attr("id");
-    this.current = 0;
-    this.total = 0;
-    this.nextButton = this.container.find(this.options.next);
-    this.prevButton = this.container.find(this.options.prev);
-    this.retreatButton = this.container.find(this.options.retreat);
-    this.controls = this.container.find(this.options.progress.controls);
-    this.dots = this.container.find(this.options.dots.container);
-    this._initDefaults();
-    return this._initControls();
-  }
-
-  _initDefaults() {
-    this.steps.not(this.initialStep).removeClass("active");
-    this._activateInitialControls();
-    if (!this.wizard.length) {
-      return;
-    }
-    return this._calculateTotal();
-  }
-
-  _initControls() {
-    this.initialStep.find(".step-form input").on("change", (event) => {
-      this.wizard = event.target.id;
-      this._calculateTotal();
-      return this._activateInitialControls();
-    });
-    this.nextButton.on("click", (event) => {
-      this._activateNext();
-      return event.preventDefault();
-    });
-    this.prevButton.on("click", (event) => {
-      this._activatePrev();
-      return event.preventDefault();
-    });
-    return this.retreatButton.on("click", (event) => {
-      this._activateInitialStep();
-      return event.preventDefault();
-    });
-  }
-
-  _calculateTotal() {
-    this.total = this.container.find(`.${this.wizard}`).length;
-    this._updateTotalIndicator();
-    return this._buildDots();
-  }
-
-  _activateByIndex(index) {
-    var step;
-    if (index === 0) {
-      return this._activateInitialStep();
-    }
-    this.current = index;
-    step = this.container.find(`.${this.wizard}`).eq(this.current - 1);
-    return this._activateStep(step);
-  }
-
-  _activateNext() {
-    var step;
-    if (!this.wizard.length) {
-      return false;
-    }
-    step = this.container.find(`.${this.wizard}`).eq(this.current);
-    if (step.length) {
-      this.current += 1;
-      this._activateStep(step);
-      this._activateConrols();
-      if (this.current === this.total) {
-        return this._activateFinishControls();
+      if (typeof options === 'string') {
+        return data[options]();
       }
-    }
-  }
+    });
+  };
 
-  _activatePrev() {
-    var step;
-    if (!this.wizard.length) {
-      return false;
+  Calculator = class Calculator {
+    constructor(dom, options = {}) {
+      var defaults;
+      this.dom = dom;
+      defaults = {
+        step: ".step",
+        next: ".next",
+        prev: ".prev",
+        retreat: ".retreat",
+        progress: {
+          controls: ".component-calculator__forms_controls",
+          current: ".component-calculator__forms_controls .current",
+          total: ".component-calculator__forms_controls .total"
+        },
+        dots: {
+          container: '.steps-dots',
+          item: 'span'
+        }
+      };
+      this.options = $.extend(defaults, options);
+      this._setup();
     }
-    this._activateConrols();
-    if (this.current - 2 < 0) {
-      return this._activateInitialStep();
-    } else {
-      step = this.container.find(`.${this.wizard}`).eq(this.current - 2);
-      this.current -= 1;
+
+    _setup() {
+      this.container = $(this.dom);
+      this.initialStep = this.container.find(`${this.options.step}.active`);
+      this.steps = this.container.find(this.options.step);
+      this.wizard = this.initialStep.find(".step-form input:checked").attr("id");
+      this.current = 0;
+      this.total = 0;
+      this.nextButton = this.container.find(this.options.next);
+      this.prevButton = this.container.find(this.options.prev);
+      this.retreatButton = this.container.find(this.options.retreat);
+      this.controls = this.container.find(this.options.progress.controls);
+      this.dots = this.container.find(this.options.dots.container);
+      this._initDefaults();
+      return this._initControls();
+    }
+
+    _initDefaults() {
+      this.steps.not(this.initialStep).removeClass("active");
+      this._activateInitialControls();
+      if (!this.wizard.length) {
+        return;
+      }
+      return this._calculateTotal();
+    }
+
+    _initControls() {
+      this.initialStep.find(".step-form input").on("change", (event) => {
+        this.wizard = event.target.id;
+        this._calculateTotal();
+        return this._activateInitialControls();
+      });
+      this.nextButton.on("click", (event) => {
+        this._activateNext();
+        return event.preventDefault();
+      });
+      this.prevButton.on("click", (event) => {
+        this._activatePrev();
+        return event.preventDefault();
+      });
+      return this.retreatButton.on("click", (event) => {
+        this._activateInitialStep();
+        return event.preventDefault();
+      });
+    }
+
+    _calculateTotal() {
+      this.total = this.container.find(`.${this.wizard}`).length;
+      this._updateTotalIndicator();
+      return this._buildDots();
+    }
+
+    _activateByIndex(index) {
+      var step;
+      if (index === 0) {
+        return this._activateInitialStep();
+      }
+      this.current = index;
+      step = this.container.find(`.${this.wizard}`).eq(this.current - 1);
       return this._activateStep(step);
     }
-  }
 
-  _activateStep(step) {
-    this.steps.removeClass('active');
-    $(step).addClass('active');
-    this._activateDot();
-    return this._updateCurrentIndicator();
-  }
-
-  _activateInitialStep() {
-    this.current = 0;
-    this._activateStep(this.initialStep);
-    return this._activateInitialControls();
-  }
-
-  _activateConrols() {
-    this.controls.show();
-    this.nextButton.show();
-    this.prevButton.removeClass("disabled").show();
-    return this.retreatButton.hide();
-  }
-
-  _deactivateControls() {
-    this.nextButton.hide();
-    this.prevButton.hide();
-    return this.retreatButton.show();
-  }
-
-  _activateInitialControls() {
-    this._activateConrols();
-    return this.prevButton.addClass("disabled");
-  }
-
-  _activateFinishControls() {
-    this._deactivateControls();
-    return this.controls.hide();
-  }
-
-  _updateCurrentIndicator() {
-    return $(this.options.progress.current).text(this.current + 1);
-  }
-
-  _updateTotalIndicator() {
-    return $(this.options.progress.total).text(this.total);
-  }
-
-  _buildDots() {
-    var index, self;
-    self = this;
-    this.dots.empty();
-    index = 0;
-    while (index < this.total) {
-      this.dots.append(`<span class='${(index === 0 ? "active" : "")}'></span>`);
-      index++;
+    _activateNext() {
+      var step;
+      if (!this.wizard.length) {
+        return false;
+      }
+      step = this.container.find(`.${this.wizard}`).eq(this.current);
+      if (step.length) {
+        this.current += 1;
+        this._activateStep(step);
+        this._activateConrols();
+        if (this.current === this.total) {
+          return this._activateFinishControls();
+        }
+      }
     }
-    return this.dots.find(this.options.dots.item).off("click").on("click", function() {
-      return self._activateByIndex($(this).index());
-    });
-  }
 
-  _activateDot() {
-    var currentDot, dots;
-    dots = this.dots.find(this.options.dots.item);
-    currentDot = dots.eq(this.current);
-    dots.removeClass('active');
-    return currentDot.addClass('active');
-  }
+    _activatePrev() {
+      var step;
+      if (!this.wizard.length) {
+        return false;
+      }
+      this._activateConrols();
+      if (this.current - 2 < 0) {
+        return this._activateInitialStep();
+      } else {
+        step = this.container.find(`.${this.wizard}`).eq(this.current - 2);
+        this.current -= 1;
+        return this._activateStep(step);
+      }
+    }
 
-};
+    _activateStep(step) {
+      this.steps.removeClass('active');
+      $(step).addClass('active');
+      this._activateDot();
+      return this._updateCurrentIndicator();
+    }
 
-window.Calculator = Calculator;
+    _activateInitialStep() {
+      this.current = 0;
+      this._activateStep(this.initialStep);
+      return this._activateInitialControls();
+    }
+
+    _activateConrols() {
+      this.controls.show();
+      this.nextButton.show();
+      this.prevButton.removeClass("disabled").show();
+      return this.retreatButton.hide();
+    }
+
+    _deactivateControls() {
+      this.nextButton.hide();
+      this.prevButton.hide();
+      return this.retreatButton.show();
+    }
+
+    _activateInitialControls() {
+      this._activateConrols();
+      return this.prevButton.addClass("disabled");
+    }
+
+    _activateFinishControls() {
+      this._deactivateControls();
+      return this.controls.hide();
+    }
+
+    _updateCurrentIndicator() {
+      return $(this.options.progress.current).text(this.current + 1);
+    }
+
+    _updateTotalIndicator() {
+      return $(this.options.progress.total).text(this.total);
+    }
+
+    _buildDots() {
+      var index, self;
+      self = this;
+      this.dots.empty();
+      index = 0;
+      while (index < this.total) {
+        this.dots.append(`<span class='${(index === 0 ? "active" : "")}'></span>`);
+        index++;
+      }
+      return this.dots.find(this.options.dots.item).off("click").on("click", function() {
+        return self._activateByIndex($(this).index());
+      });
+    }
+
+    _activateDot() {
+      var currentDot, dots;
+      dots = this.dots.find(this.options.dots.item);
+      currentDot = dots.eq(this.current);
+      dots.removeClass('active');
+      return currentDot.addClass('active');
+    }
+
+  };
+
+  window.Calculator = Calculator;
+
+}).call(this);
--- dist/assets/js/main.js
+++ dist/assets/js/main.js
@@ -1,295 +1,298 @@
-var cabinetAccordion, initAccordion, initCarousel, initFancyBox, initMenu, initPrices, initProgress, initSteps, initTabs, initUI, initVideoSHow;
+(function() {
+  var cabinetAccordion, initAccordion, initCarousel, initFancyBox, initMenu, initPrices, initProgress, initSteps, initTabs, initUI, initVideoSHow;
 
-initCarousel = function() {
-  $('.component-advantages .content-wrapper').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToShow: 1,
-          slidesToScroll: 1
+  initCarousel = function() {
+    $('.component-advantages .content-wrapper').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
         }
-      }
-    ]
-  });
-  $('.component-popular__projects .projects-block').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  $('.objects-slides .objects-block').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToShow: 1,
-          slidesToScroll: 1
-        }
-      }
-    ]
-  });
-  $('.works-grid').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToScroll: 1,
-          rows: 2,
-          slidesPerRow: 1
-        }
-      }
-    ]
-  });
-  $('.component-steps__work .component-steps-content').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToScroll: 1,
-          slidesToShow: 1,
-          adaptiveHeight: true
-        }
-      }
-    ]
-  });
-  return $('.our-advantages_slider').slick({
-    responsive: [
-      {
-        breakpoint: 2560,
-        settings: "unslick"
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          dots: true,
-          arrows: false,
-          infinite: true,
-          speed: 600,
-          slidesToScroll: 1,
-          slidesToShow: 1
-        }
-      }
-    ]
-  });
-};
-
-initMenu = function() {
-  $('.menu-btn').on('click', function() {
-    return $('body').toggleClass('menu-opened');
-  });
-  return $(document).mouseup(function(e) {
-    var menu;
-    menu = $('.header__navigation-block, .menu-btn');
-    if (!menu.is(e.target) && menu.has(e.target).length === 0) {
-      $('body').removeClass('menu-opened');
-    }
-  });
-};
-
-initVideoSHow = function() {
-  return $('.video_bg').on('click', function() {
-    return $(this).addClass('hide');
-  });
-};
-
-initAccordion = function() {
-  $('.accordion-item__content').slideUp();
-  $('.open-start').find('.accordion-item__content').stop().slideDown();
-  return $('.accordion-item__title').on('click', function(e) {
-    $(this).closest('.accordion-item').toggleClass('active');
-    if ($('.accordion-item').hasClass('active')) {
-      return $(this).closest('.accordion-item__title').next('.accordion-item__content').stop().slideToggle();
-    } else {
-      return $(this).closest('.accordion-item__title').next('.accordion-item__content').stop().slideUp();
-    }
-  });
-};
-
-cabinetAccordion = function() {
-  if (window.innerWidth < 640) {
-    console.log(window.innerwidth);
-    $('.cabinet_description-info').slideUp();
-    $('.cabinet_description-sub-info .image, .cabinet_description-sub-info_text_item').slideUp();
-    return $('.show-info').on('click', function(e) {
-      $(this).toggleClass('active');
-      $('.cabinet_description-info').slideToggle();
-      return $('.cabinet_description-sub-info .image, .cabinet_description-sub-info_text_item').slideToggle();
+      ]
     });
-  }
-};
-
-initFancyBox = function() {
-  $(".video > a, .project-visual__item a, .gallery-item a, .build-gallery a").fancybox({
-    arrows: true,
-    infobar: false,
-    toolbar: false,
-    transitionEffect: "fade",
-    thumbs: {
-      autoStart: true,
-      axis: 'x'
-    }
-  });
-  return $(".call-me > a").fancybox({
-    arrows: false,
-    infobar: false,
-    toolbar: false
-  });
-};
-
-initSteps = function() {
-  var form, stepBack;
-  form = $("#steps-calc");
-  form.validate({
-    errorPlacement: function(error, element) {
-      return element.before(error);
-    },
-    rules: {
-      required: true
-    }
-  });
-  stepBack = function() {
-    return false;
+    $('.component-popular__projects .projects-block').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    $('.objects-slides .objects-block').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToShow: 1,
+            slidesToScroll: 1
+          }
+        }
+      ]
+    });
+    $('.works-grid').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToScroll: 1,
+            rows: 2,
+            slidesPerRow: 1
+          }
+        }
+      ]
+    });
+    $('.component-steps__work .component-steps-content').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToScroll: 1,
+            slidesToShow: 1,
+            adaptiveHeight: true
+          }
+        }
+      ]
+    });
+    return $('.our-advantages_slider').slick({
+      responsive: [
+        {
+          breakpoint: 2560,
+          settings: "unslick"
+        },
+        {
+          breakpoint: 767,
+          settings: {
+            dots: true,
+            arrows: false,
+            infinite: true,
+            speed: 600,
+            slidesToScroll: 1,
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
   };
-  stepBack = function() {
-    if ($('#steps-calc').steps('previous')) {
-      setTimeout((function() {
-        return stepBack();
-      }), 200);
-    }
-    return $('a[href="#previous"]').show();
-  };
-  return $('#steps-calc').steps({
-    headerTag: '.hidden-title',
-    bodyTag: '.step',
-    transitionEffect: 'none',
-    enablePagination: true,
-    enableFinishButton: true,
-    enableCancelButton: false,
-    enableAllSteps: true,
-    labels: {
-      next: 'Далее',
-      previous: 'Назад',
-      finish: 'Рассчитать заново'
-    },
-    onInit: function(e) {
-      $('a[href="#previous"]').addClass('previous');
-      return $('a[href="#finish"]').addClass('finish-button');
-    },
-    onFinished: function(e) {
-      return stepBack();
-    },
-    onStepChanging: function(event, currentIndex, newIndex) {
-      form.validate().settings.ignore = ':disabled,:hidden';
-      if (form.valid()) {
-        $('.steps-numbers p span').text(newIndex + 1);
+
+  initMenu = function() {
+    $('.menu-btn').on('click', function() {
+      return $('body').toggleClass('menu-opened');
+    });
+    return $(document).mouseup(function(e) {
+      var menu;
+      menu = $('.header__navigation-block, .menu-btn');
+      if (!menu.is(e.target) && menu.has(e.target).length === 0) {
+        $('body').removeClass('menu-opened');
       }
-      if (newIndex === $('.wizard .step').length - 1) {
-        $('.component-calculator__forms_controls, .steps.clearfix').css({
-          position: 'absolute',
-          visibility: 'hidden',
-          opacity: '0'
-        });
-        $('.previous').hide();
+    });
+  };
+
+  initVideoSHow = function() {
+    return $('.video_bg').on('click', function() {
+      return $(this).addClass('hide');
+    });
+  };
+
+  initAccordion = function() {
+    $('.accordion-item__content').slideUp();
+    $('.open-start').find('.accordion-item__content').stop().slideDown();
+    return $('.accordion-item__title').on('click', function(e) {
+      $(this).closest('.accordion-item').toggleClass('active');
+      if ($('.accordion-item').hasClass('active')) {
+        return $(this).closest('.accordion-item__title').next('.accordion-item__content').stop().slideToggle();
       } else {
-        $('.component-calculator__forms_controls, .steps.clearfix').css({
-          opacity: '1',
-          visibility: 'visible'
-        });
+        return $(this).closest('.accordion-item__title').next('.accordion-item__content').stop().slideUp();
       }
-      if (currentIndex < newIndex) {
-        form.find('.body:eq(' + newIndex + ') label.error').remove();
-        form.find('.body:eq(' + newIndex + ') .error').removeClass('error');
-      }
-      return form.valid();
+    });
+  };
+
+  cabinetAccordion = function() {
+    if (window.innerWidth < 640) {
+      console.log(window.innerwidth);
+      $('.cabinet_description-info').slideUp();
+      $('.cabinet_description-sub-info .image, .cabinet_description-sub-info_text_item').slideUp();
+      return $('.show-info').on('click', function(e) {
+        $(this).toggleClass('active');
+        $('.cabinet_description-info').slideToggle();
+        return $('.cabinet_description-sub-info .image, .cabinet_description-sub-info_text_item').slideToggle();
+      });
     }
+  };
+
+  initFancyBox = function() {
+    $(".video > a, .project-visual__item a, .gallery-item a, .build-gallery a").fancybox({
+      arrows: true,
+      infobar: false,
+      toolbar: false,
+      transitionEffect: "fade",
+      thumbs: {
+        autoStart: true,
+        axis: 'x'
+      }
+    });
+    return $(".call-me > a").fancybox({
+      arrows: false,
+      infobar: false,
+      toolbar: false
+    });
+  };
+
+  initSteps = function() {
+    var form, stepBack;
+    form = $("#steps-calc");
+    form.validate({
+      errorPlacement: function(error, element) {
+        return element.before(error);
+      },
+      rules: {
+        required: true
+      }
+    });
+    stepBack = function() {
+      return false;
+    };
+    stepBack = function() {
+      if ($('#steps-calc').steps('previous')) {
+        setTimeout((function() {
+          return stepBack();
+        }), 200);
+      }
+      return $('a[href="#previous"]').show();
+    };
+    return $('#steps-calc').steps({
+      headerTag: '.hidden-title',
+      bodyTag: '.step',
+      transitionEffect: 'none',
+      enablePagination: true,
+      enableFinishButton: true,
+      enableCancelButton: false,
+      enableAllSteps: true,
+      labels: {
+        next: 'Далее',
+        previous: 'Назад',
+        finish: 'Рассчитать заново'
+      },
+      onInit: function(e) {
+        $('a[href="#previous"]').addClass('previous');
+        return $('a[href="#finish"]').addClass('finish-button');
+      },
+      onFinished: function(e) {
+        return stepBack();
+      },
+      onStepChanging: function(event, currentIndex, newIndex) {
+        form.validate().settings.ignore = ':disabled,:hidden';
+        if (form.valid()) {
+          $('.steps-numbers p span').text(newIndex + 1);
+        }
+        if (newIndex === $('.wizard .step').length - 1) {
+          $('.component-calculator__forms_controls, .steps.clearfix').css({
+            position: 'absolute',
+            visibility: 'hidden',
+            opacity: '0'
+          });
+          $('.previous').hide();
+        } else {
+          $('.component-calculator__forms_controls, .steps.clearfix').css({
+            opacity: '1',
+            visibility: 'visible'
+          });
+        }
+        if (currentIndex < newIndex) {
+          form.find('.body:eq(' + newIndex + ') label.error').remove();
+          form.find('.body:eq(' + newIndex + ') .error').removeClass('error');
+        }
+        return form.valid();
+      }
+    });
+  };
+
+  initProgress = function() {
+    var progressValue;
+    progressValue = $('.project-progress').attr('data-progress');
+    $('.percentage').css("left", progressValue + "%");
+    $('.percentage span').text(progressValue);
+    return $('.line__bg').css("width", progressValue + "%");
+  };
+
+  initUI = function() {
+    return $('select').select2({
+      minimumResultsForSearch: -1
+    });
+  };
+
+  initPrices = function() {
+    return $('#steps-calc').calculator();
+  };
+
+  initTabs = function() {
+    return $('.tab-nav').on('click', function(e) {
+      var tabId;
+      e.preventDefault();
+      tabId = $(this).attr('href');
+      $('.tab-item').hide();
+      $('.tab-nav').removeClass('active');
+      $(this).addClass('active');
+      return $('.prices-form').find(tabId).show();
+    });
+  };
+
+  $(function() {
+    initCarousel();
+    initFancyBox();
+    initMenu();
+    initVideoSHow();
+    initAccordion();
+    initPrices();
+    initProgress();
+    initUI();
+    cabinetAccordion();
+    return initTabs();
   });
-};
 
-initProgress = function() {
-  var progressValue;
-  progressValue = $('.project-progress').attr('data-progress');
-  $('.percentage').css("left", progressValue + "%");
-  $('.percentage span').text(progressValue);
-  return $('.line__bg').css("width", progressValue + "%");
-};
-
-initUI = function() {
-  return $('select').select2({
-    minimumResultsForSearch: -1
-  });
-};
-
-initPrices = function() {
-  return $('#steps-calc').calculator();
-};
-
-initTabs = function() {
-  return $('.tab-nav').on('click', function(e) {
-    var tabId;
-    e.preventDefault();
-    tabId = $(this).attr('href');
-    $('.tab-item').hide();
-    $('.tab-nav').removeClass('active');
-    $(this).addClass('active');
-    return $('.prices-form').find(tabId).show();
-  });
-};
-
-$(function() {
-  initCarousel();
-  initFancyBox();
-  initMenu();
-  initVideoSHow();
-  initAccordion();
-  initPrices();
-  initProgress();
-  initUI();
-  cabinetAccordion();
-  return initTabs();
-});
+}).call(this);
